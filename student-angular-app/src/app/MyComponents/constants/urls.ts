6b09775f46293b3fa1cc302e6195a42c8c2f6e6c
paras <paras.jain@girnarsoft.co.in>
2023-01-15 20:25:53 +0530

    updating urls

--- student-angular-app/src/app/MyComponents/constants/urls.ts
+++ student-angular-app/src/app/MyComponents/constants/urls.ts
@@ -1,3 +1,7 @@
 export const allStudentsURL:string = 'http://localhost:9999/student/all';
+export const saveStudentURL:string = 'http://localhost:9999/student';
+export const studentByIdURL:string = 'http://localhost:9999/student';
+// export const studentByIdURL:string = 'http://localhost:9999/student';
+
 export class Urls {
 }
